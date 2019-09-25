07019617f96ea6366bd9d16e4635e8cebda442b6
Geoffrey Jount <geoffrey.jount@epita.fr>
2019-09-25 13:56:38 +0200

    Script prototype for automatic configuration.

---
+++ myconfig.py
@@ -0,0 +1,16 @@
+# Install packages
+## Custom terminal
+## rofi
+## chromium
+## vim
+## git
+
+
+# Set configuration files
+## pictures
+## .bashrc
+## .vimrc
+## .config/i3/config
+
+
+# Reload Mod + Shift + R
