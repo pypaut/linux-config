c95ee861a14d1b38cb197967db9a8595e897f7fe
Geoffrey Jount <geoffrey.jount@epita.fr>
2020-09-22 18:29:59 +0200

    setup.py

---
+++ setup.py
@@ -0,0 +1,71 @@
+#!/bin/sh
+
+# Packages
+echo '##### INSTALLING PACKAGES...'
+sudo apt install -y \
+        autoconf \
+        autoconf \
+        automake \
+        build-essential \
+        compton \
+        feh \
+        git \
+        i3lock \
+        i3status \
+        libev-dev \
+        libev-dev \
+        libpango1.0-dev \
+        libsnack2-dev \
+        libstartup-notification0-dev \
+        libxcb-cursor-dev \
+        libxcb-icccm4-dev \
+        libxcb-keysyms1-dev \
+        libxcb-randr0-dev \
+        libxcb-shape0-dev \
+        libxcb-util0-dev \
+        libxcb-xinerama0-dev \
+        libxcb-xkb-dev \
+        libxcb-xrm-dev \
+        libxcb-xrm0 \
+        libxcb1-dev \
+        libxkbcommon-dev \
+        libxkbcommon-x11-dev \
+        libyajl-dev \
+        neovim \
+        rofi \
+        tmux
+
+
+# Config files
+echo '##### COPYING CONFIGURATION FILES...'
+mkdir -p ~/.config/i3/ \
+        ~/.config/i3status/ \
+        ~/.config/rofi \
+        ~/.config/nvim \
+        ~/.config/gtk-3.0
+
+cp .config/gtk-3.0/settings.ini ~/.config/gtk-3.0/settings.ini
+cp .config/nvim/init.vim        ~/.config/nvim/init.vim
+cp .config/i3/config            ~/.config/i3/config
+cp .config/i3status/config      ~/.config/i3status/config
+cp .config/rofi/config          ~/.config/rofi/config
+
+cp .tmux.conf .compton.conf .bashrc ~/
+
+
+# i3gaps
+echo '##### INSTALLING i3gaps...'
+git clone https://www.github.com/Airblader/i3 ~/Documents/i3-gaps \
+&& cd ~/Documents/i3-gaps
+autoreconf --force --install
+rm -rf build/ && mkdir -p build && cd build/
+../configure --prefix=/usr --sysconfdir=/etc --disable-sanitizers
+make -j8 && sudo make install
+
+
+# st
+echo '##### INSTALLING SIMPLE TERMINAL...'
+cd ~/Documents
+git clone https://github.com/pypaut/st.git && cd st
+make && sudo make clean install
+
