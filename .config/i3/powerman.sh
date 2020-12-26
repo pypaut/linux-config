96e43bfde832579a849b904a31d22d2774bb4c84
Geoffrey Jount <geoffrey.jount@epita.fr>
2020-12-26 22:25:49 +0100

    [i3] More scripts & Rofi keybind
    Scripts : dropbox, rofi

--- .config/i3/powerman.sh
+++ .config/i3/powerman.sh
@@ -2,6 +2,6 @@
 
 i3-nagbar -t warning \
     -m 'So, what do you want today?' \
-    -b 'Lock' '$HOME/.config/i3/i3lock.sh' \
+    -b 'Lock' "$HOME/.config/i3/i3lock.sh" \
     -b 'Logout' 'i3-msg exit' \
     -b 'Shutdown' 'shutdown 0'
