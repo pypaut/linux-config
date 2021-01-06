25c7f48fb3c985f48eb72b34785ca3c52e901ff3
Geoffrey Jount <geoffrey.jount@epita.fr>
2021-01-06 16:18:06 +0100

    [i3] Fix touchpad script

--- .config/i3/touchpad.sh
+++ .config/i3/touchpad.sh
@@ -11,12 +11,12 @@ echo "TrackPoint ID is $track_id"
 touch_natural_scrolling_id=`xinput list-props ${touch_id} | \
                       grep -i "Natural Scrolling Enabled (" \
                       | cut -d'(' -f2 | cut -d')' -f1`
-                      echo "Natural scrolling ID (touchpad) is ${natural_scrolling_id}"
+                      echo "Natural scrolling ID (touchpad) is ${touch_natural_scrolling_id}"
 
 track_natural_scrolling_id=`xinput list-props ${track_id} | \
                       grep -i "Natural Scrolling Enabled (" \
                       | cut -d'(' -f2 | cut -d')' -f1`
-                      echo "Natural scrolling ID (trackpoint) is ${natural_scrolling_id}"
+                      echo "Natural scrolling ID (trackpoint) is ${track_natural_scrolling_id}"
 
 tapping_id=`xinput list-props ${touch_id} | \
                       grep -i "Tapping Enabled (" \
@@ -32,4 +32,4 @@ echo "Disable While Typing ID is ${disable_while_typing_id}"
 xinput --set-prop ${touch_id} ${touch_natural_scrolling_id} 1
 xinput --set-prop ${touch_id} ${tapping_id} 1
 xinput --set-prop ${touch_id} ${disable_while_typing_id} 0
-xinput --set-prop ${track_id} ${track_natural_scrolling_id} 0
+xinput --set-prop ${track_id} ${track_natural_scrolling_id} 1
