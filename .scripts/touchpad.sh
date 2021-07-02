3d7d41e8a908eaa5c2f8523abe0a2893ab7a0711
Geoffrey <pypaut@hotmail.fr>
2021-07-02 09:35:09 +0200

    Touchpad script : disable touchpad

--- .scripts/touchpad.sh
+++ .scripts/touchpad.sh
@@ -29,6 +29,7 @@ disable_while_typing_id=`xinput list-props ${touch_id} | \
 echo "Disable While Typing ID is ${disable_while_typing_id}"
 
 # Set properties
+xinput --set-prop ${touch_id} "Device Enabled" 0
 xinput --set-prop ${touch_id} ${touch_natural_scrolling_id} 1
 xinput --set-prop ${touch_id} ${tapping_id} 1
 xinput --set-prop ${touch_id} ${disable_while_typing_id} 0
