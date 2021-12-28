36fed08e1d1b23c77012de50bc593138ca5bdc85
Geoffrey <pypaut@hotmail.fr>
2021-12-28 15:06:14 +0100

    [i3] Sleep cmd + autostart update

--- .config/i3/autostart.sh
+++ .config/i3/autostart.sh
@@ -5,4 +5,7 @@ $HOME/.scripts/dropbox.sh
 $HOME/.scripts/nm-applet.sh
 $HOME/.scripts/polybar.sh
 $HOME/.scripts/touchpad.sh
-$HOME/.scripts/ibus.sh
+# $HOME/.scripts/ibus.sh
+$HOME/.scripts/compton.sh
+# $HOME/.scripts/display.sh
+$HOME/.scripts/batterycheck.sh
