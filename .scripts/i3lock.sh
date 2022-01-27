6d64988522c0e488eedae6e6341a3cda7e8655cf
Geoffrey <pypaut@hotmail.fr>
2022-01-27 19:50:46 +0100

    nothing really

--- .scripts/i3lock.sh
+++ .scripts/i3lock.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 kill -SIGUSR1 $(pidof dunst) && \
-i3lock -t -n -i $HOME/Pictures/System/lock.png && \
+i3lock -t -n -i $HOME/Pictures/System/saber.png && \
 kill -SIGUSR2 $(pidof dunst)
