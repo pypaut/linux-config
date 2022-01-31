ce01809d717193a935037006d1f1d12b57ce7179
Geoffrey <pypaut@hotmail.fr>
2022-01-31 12:03:23 +0100

    [i3] Polybar : monitor module

--- .scripts/monitor.sh
+++ .scripts/monitor.sh
@@ -1,27 +1,32 @@
 #!/bin/bash
-EXTERNAL_OUTPUT="DisplayPort-0"
-INTERNAL_OUTPUT="eDP"
+
+EXTERNAL_OUTPUT="HDMI-2"
+INTERNAL_OUTPUT="eDP-1"
 
 # if we don't have a file, start at zero
 if [ ! -f "/tmp/monitor_mode.dat" ] ; then
-  monitor_mode="all"
+  monitor_mode="MIXED"
 
 # otherwise read the value from the file
 else
   monitor_mode=`cat /tmp/monitor_mode.dat`
 fi
 
-if [ $monitor_mode = "all" ]; then
-        monitor_mode="EXTERNAL"
-        xrandr --output $INTERNAL_OUTPUT --off --output $EXTERNAL_OUTPUT --auto
-elif [ $monitor_mode = "EXTERNAL" ]; then
-        monitor_mode="INTERNAL"
-        xrandr --output $INTERNAL_OUTPUT --auto --output $EXTERNAL_OUTPUT --off
-elif [ $monitor_mode = "INTERNAL" ]; then
-        monitor_mode="CLONES"
-        xrandr --output $INTERNAL_OUTPUT --auto --output $EXTERNAL_OUTPUT --auto --same-as $INTERNAL_OUTPUT
-else
-        monitor_mode="all"
-        xrandr --output $INTERNAL_OUTPUT --auto --output $EXTERNAL_OUTPUT --auto --left-of $INTERNAL_OUTPUT
+if [ "$1" = "change" ]; then
+    if [ $monitor_mode = "MIXED" ]; then
+            monitor_mode="EXTERNAL"
+            xrandr --output $INTERNAL_OUTPUT --off --output $EXTERNAL_OUTPUT --auto
+    elif [ $monitor_mode = "EXTERNAL" ]; then
+            monitor_mode="INTERNAL"
+            xrandr --output $INTERNAL_OUTPUT --auto --output $EXTERNAL_OUTPUT --off
+    elif [ $monitor_mode = "INTERNAL" ]; then
+            monitor_mode="CLONES"
+            xrandr --output $INTERNAL_OUTPUT --auto --output $EXTERNAL_OUTPUT --auto --same-as $INTERNAL_OUTPUT
+    else
+            monitor_mode="MIXED"
+            xrandr --output $INTERNAL_OUTPUT --auto --output $EXTERNAL_OUTPUT --auto --left-of $INTERNAL_OUTPUT
+    fi
+    echo "${monitor_mode}" > /tmp/monitor_mode.dat
 fi
-echo "${monitor_mode}" > /tmp/monitor_mode.dat
+
+echo "${monitor_mode}"
