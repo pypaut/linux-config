be177d81dbef84901540f8b7db0a46a0a3e55429
Geoffrey <pypaut@hotmail.fr>
2022-01-31 17:41:44 +0100

    Update monitor script

--- .scripts/monitor.sh
+++ .scripts/monitor.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
-EXTERNAL_OUTPUT="HDMI-2"
+EXTERNAL_OUTPUT="DP-2"
 INTERNAL_OUTPUT="eDP-1"
+
+EXTERNAL_RES="1920x1080"
+INTERNAL_RES="1920x1080"
+
+EXTERNAL_POS="left-of"
+
 
 # if we don't have a file, start at zero
 if [ ! -f "/tmp/monitor_mode.dat" ] ; then
-  monitor_mode="MIX"
+  monitor_mode="INT"
 
 # otherwise read the value from the file
 else
@@ -15,16 +21,20 @@ fi
 if [ "$1" = "change" ]; then
     if [ $monitor_mode = "MIX" ]; then
             monitor_mode="EXT"
-            xrandr --output $INTERNAL_OUTPUT --off --output $EXTERNAL_OUTPUT --auto
+            xrandr --output $INTERNAL_OUTPUT --off --output $EXTERNAL_OUTPUT --mode $EXTERNAL_RES
     elif [ $monitor_mode = "EXT" ]; then
             monitor_mode="INT"
-            xrandr --output $INTERNAL_OUTPUT --auto --output $EXTERNAL_OUTPUT --off
+            xrandr --output $INTERNAL_OUTPUT --mode $INTERNAL_RES --output $EXTERNAL_OUTPUT --off
     elif [ $monitor_mode = "INT" ]; then
             monitor_mode="CP"
-            xrandr --output $INTERNAL_OUTPUT --auto --output $EXTERNAL_OUTPUT --auto --same-as $INTERNAL_OUTPUT
+            xrandr --output $INTERNAL_OUTPUT --mode $INTERNAL_RES \
+                --output $EXTERNAL_OUTPUT --mode $EXTERNAL_RES \
+                --same-as $INTERNAL_OUTPUT
     else
             monitor_mode="MIX"
-            xrandr --output $INTERNAL_OUTPUT --auto --output $EXTERNAL_OUTPUT --auto --left-of $INTERNAL_OUTPUT
+            xrandr --output $INTERNAL_OUTPUT --mode $INTERNAL_RES \
+                --output $EXTERNAL_OUTPUT --mode $EXTERNAL_RES \
+                --$EXTERNAL_POS $INTERNAL_OUTPUT
     fi
     echo "${monitor_mode}" > /tmp/monitor_mode.dat
 fi
