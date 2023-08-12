7554484d2113b46465b5350c47a4a0d1ae546a4e
Geoffrey JOUNT <gjount@scaleway.com>
2023-08-12 10:49:38 +0200

    Sound notifications

--- .scripts/sound.sh
+++ .scripts/sound.sh
@@ -2,6 +2,7 @@
 
 
 sink=$(pacmd list-sinks | awk '/\*/,EOF {print $3; exit}')
+dunst_id=10
 
 mute_vol() {
     pactl set-sink-mute ${sink} toggle
@@ -15,9 +16,12 @@ increase_vol() {
     pactl set-sink-volume ${sink} +5%
 }
 
+# TODO: strikethrough text when muted (instead of "mute: yes")
 notify_vol() {
     vol=$(pactl get-sink-volume ${sink} | awk '{print $5}')
-    notify-send "VOLUME: $vol"
+    mute_status=$(pactl get-sink-mute ${sink} | awk '{ print $2 }')
+    dunstify -C ${dunst_id}
+    dunstify -r ${dunst_id} "VOLUME: $vol" "MUTED: ${mute_status}"
 }
 
 case "$1" in
