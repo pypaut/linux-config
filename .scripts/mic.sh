865f08867b875fa79c2595d65a469585db40a29e
Geoffrey JOUNT <gjount@scaleway.com>
2024-02-02 11:35:05 +0100

    Enhance mic script

--- .scripts/mic.sh
+++ .scripts/mic.sh
@@ -2,10 +2,22 @@
 
 
 # source="alsa_input.pci-0000_00_1f.3.analog-stereo"
-source=$(pacmd list-sources | awk '/\*/,EOF {print $3; exit}')
+# source=$(pacmd list-sources | awk '/\*/,EOF {print $3; exit}') # idk but it works
+# source=1 # internal
+source=5 # external webcam
+dunst_id=10
 
 mute_vol() {
     pactl set-source-mute ${source} toggle
+
+    mute_status=$(pactl get-source-mute ${source} | awk '{ print $2 }')
+    dunstify -C ${dunst_id}
+
+    if [[ $mute_status = "no" ]]; then
+        dunstify -r ${dunst_id} "Microphone" "ON"
+    else
+        dunstify -r ${dunst_id} "Microphone" "<s>OFF</s>"
+    fi
 }
 
 decrease_vol() {
