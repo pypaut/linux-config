9d28bf6f034223f169d498a5df563abb3c64dafb
Geoffrey <geoffreyjount@proton.me>
2026-01-12 22:13:49 +0100

    fix(nvim): auto fmt go files

--- .config/nvim/lua/pypaut/auto.lua
+++ .config/nvim/lua/pypaut/auto.lua
@@ -1,5 +1,17 @@
+function GoCallbacks()
+    -- Fix imports
+    local cur = vim.fn.getpos('.')
+    vim.api.nvim_command('%!goimports')
+    vim.fn.setpos('.', cur)
+
+    -- Run fmt
+    vim.lsp.buf.format()
+
+end
+
+
 vim.api.nvim_create_autocmd({'BufWritePre'}, {
-  pattern = {'*.go'},
-  -- buffer = 0, -- if 0 doesn't work do vim.api.nvim_get_current_buf()
-  command = "%! goimports && go fmt %:p",
+    pattern = '*.go',
+    -- buffer = 0, -- if 0 doesn't work do vim.api.nvim_get_current_buf()
+    callback = GoCallbacks,
 })
